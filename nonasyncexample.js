87c6e29fa4790d5f9fb33d64e2821af9f4e3f112
anambiar <amith.nmbr@gmail.com>
2013-07-31 16:14:55 +1000

    Adding more functions to the example

--- nonasyncexample.js
+++ nonasyncexample.js
@@ -1,3 +1,18 @@
+    function download(link, callback) {
+        // download data 
+        callback(null, results);
+    }
+
+    function index(results, callback) {
+        var indexedresults = // index the results
+        callback(null, indexedresults);
+    }
+
+    function store(results, callback) {
+            db.save(results, function(err, results) {
+                callback(err, results);
+            }
+    }
     
     // without async 
     function downloadIndexStoreLink(link, function(link, callback) {
@@ -6,11 +21,7 @@
                 index(result, function(err, result) {
                     if(!err) {
                         store(vehicle, function(err, result) {
-                            // this is where we have to store the results to the database 
-                            save(result, function(err, result) {    
-                                console.log('Done saving results ');
-                                // reached save finally and calling the actuall callback now 
-                                callback(err, result);
+                                callback(err, result);                      
                             });
                         });
                     }
@@ -19,10 +30,12 @@
         });
     });
 
-    downloadIndexStoreLink('http://downloader.com/some.pdf');
+    downloadIndexStoreLink('http://downloader.com/some.pdf', function(err, results){
+        console.log('Done');
+    });
     
     // The problem is you cannot do something like below, unless they are synchronous functions 
-    // which will then block the event loop and hence the whole application
+    // which will then block the event loop and the whole application
     
     download(link)
     index();
